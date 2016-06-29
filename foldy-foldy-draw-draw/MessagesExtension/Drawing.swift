f255d9d735ae9e7299018c2fb4a4c315604bc06c
Michiel Boertjes <michiel@noodlewerk.com>
2016-06-29 21:30:18 +0200

    extends drawing and nwadrawing so they can be initialised with json and can be exported to json

--- foldy-foldy-draw-draw/MessagesExtension/Drawing.swift
+++ foldy-foldy-draw-draw/MessagesExtension/Drawing.swift
@@ -29,4 +29,78 @@ extension Drawing {
 
     }
 }
+
+// MARK: - JSON stuff
+
+extension Drawing {
+    
+    init?(json: String)
+    {
+        self.head = nil
+        self.torso = nil
+        self.legs = nil
+        self.feet = nil
+        
+        if let jsonData = json.data(using: String.Encoding.utf8)
+        {
+            do
+            {
+                let dict = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments)
+                
+                if let headDict = dict.value(forKey: "head") as? NSDictionary
+                {
+                    self.head = NWADrawing(dict: headDict)
+                }
+                
+                if let torsoDict = dict.value(forKey: "torso") as? NSDictionary
+                {
+                    self.torso = NWADrawing(dict: torsoDict)
+                }
+                
+                if let legsDict = dict.value(forKey: "legs") as? NSDictionary
+                {
+                    self.legs = NWADrawing(dict: legsDict)
+                }
+                
+                if let feetDict = dict.value(forKey: "feet") as? NSDictionary
+                {
+                    self.feet = NWADrawing(dict: feetDict)
+                }
+                
+            } catch let error {
+                print(error)
+            }
+            
+        } else {
+            
+        }
+    }
+    
+    func toJSON() -> NSDictionary
+    {
+        let dict = NSDictionary()
+        if self.head != nil
+        {
+            dict.setValue(self.head?.toJSON(), forKey: "head")
+        }
+        
+        if self.torso != nil
+        {
+            dict.setValue(self.torso?.toJSON(), forKey: "torso")
+        }
+        
+        if self.legs != nil
+        {
+            dict.setValue(self.legs?.toJSON(), forKey: "legs")
+        }
+        
+        if self.feet != nil
+        {
+            dict.setValue(self.feet?.toJSON(), forKey: "feet")
+        }
+        
+        return dict
+    }
+}
+
     
