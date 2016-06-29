157ad86b84f144a167475abc133e51af2f8be5be
Oskar Smith <oskar@fonk.amsterdam>
2016-06-29 20:45:25 +0200

    Add outlets for the build drawing view controller

--- foldy-foldy-draw-draw/MessagesExtension/BuildDrawingViewController.swift
+++ foldy-foldy-draw-draw/MessagesExtension/BuildDrawingViewController.swift
@@ -5,4 +5,10 @@ class BuildDrawingViewController: UIViewController {
     static let storyboardIdentifier = "BuildDrawingViewController"
 
     var drawing: Drawing?
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    @IBAction func foldButtonPressed(_ sender: AnyObject) {
+    }
 }
